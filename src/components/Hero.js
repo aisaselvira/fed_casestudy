9de559ce2b1d3b46f97f445dd9757a05d66df0c4
Aisa Selvira <aisaselvira2020@gmail.com>
2024-08-15 10:47:38 +0700

    blog

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -20,9 +20,25 @@ const Hero = () => {
             Mencetak generasi unggul yang mahir di bidang teknologi digital
             dan teguh dalam pemahaman Islam Ahlu Sunnah Wal Jama'ah
           </p>
-          <button className="mt-6 px-4 py-2 bg-blue-700 text-white rounded-full">
-            Download Profile Kami
-          </button>
+          <div className="flex items-center mt-6 border-2 border-blue-500 rounded-full px-2 py-1">
+            <span className="text-gray-700 pr-4">
+              Jago IT Rajin Ngaji bersama Al Kautsar
+            </span>
+            <button className="flex items-center py-2 bg-blue-700 text-white rounded-full">
+              <svg
+                className="w-11 h-4"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M3.172 5.172a4 4 0 011.415-1.415l5.793-5.793a2 2 0 112.829 2.829L10.414 4h6.586a2 2 0 110 4H10v4h4a2 2 0 110 4H10v4h-4V9H3.414a1.986 1.986 0 01-.707-.293A4 4 0 013.172 5.172z"
+                  clipRule="evenodd"
+                />
+              </svg>
+              Download Profile Kami
+            </button>
+          </div>
         </div>
         <div className="flex-shrink-0">
           <Image
